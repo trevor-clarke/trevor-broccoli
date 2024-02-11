4f723ee3ac5e2cee94526ffa485b95ff61cd1108
Trevor Clarke <trevor@trevorc.ca>
2024-02-11 15:47:22 -0500

    test

--- ttl/ttl.py
+++ ttl/ttl.py
@@ -7,11 +7,11 @@ class TTL:
         self.parser = TTLParser()
         self.renderer = TTLRenderer(template_dir)
 
-    def process(self, input_file, output_file):
+    def process(self, input_file, output_file, tree=False):
         with open(input_file, "r") as i:
             tree = self.parser.parse(i.read())
-            # Assuming you want to print the tree for debugging; otherwise, remove this line.
-            self.parser.print_tree()
+            if tree:
+                self.parser.print_tree()
             html = self.renderer.render_document(tree)
 
         with open(output_file, "w") as o:
