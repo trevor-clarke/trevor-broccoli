26549b3945aa919020a5aa3e4016707902860eee
Trevor Clarke <trevor@trevorc.ca>
2024-02-11 15:44:13 -0500

    remove static html file, replace with auto generated

--- ttl/ttl.py
+++ ttl/ttl.py
@@ -1,13 +1,18 @@
-from ttl_parser import TTLParser
-from ttl_renderer import TTLRenderer
+from .ttl_parser import TTLParser
+from .ttl_renderer import TTLRenderer
 
 
-parser = TTLParser()
-renderer = TTLRenderer("templates")
+class TTL:
+    def __init__(self, template_dir):
+        self.parser = TTLParser()
+        self.renderer = TTLRenderer(template_dir)
 
-with open("index.ttl", "r") as i:
-    with open("index2.html", "w") as o:
-        tree = parser.parse(i.read())
-        parser.print_tree()
-        html = renderer.render_document(tree)
-        o.write(html)
+    def process(self, input_file, output_file):
+        with open(input_file, "r") as i:
+            tree = self.parser.parse(i.read())
+            # Assuming you want to print the tree for debugging; otherwise, remove this line.
+            self.parser.print_tree()
+            html = self.renderer.render_document(tree)
+
+        with open(output_file, "w") as o:
+            o.write(html)
